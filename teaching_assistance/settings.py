96cbd1282d62441f99dc07e0a2f60a8a3fd50e88
rootless <ripon1995dc@gmail.com>
2023-12-04 16:54:25 +0600

    Comment removed

--- teaching_assistance/settings.py
+++ teaching_assistance/settings.py
@@ -4,11 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6s41%h1^m94wk9c0cexzh)9o^_5@omvnto096dtnt^!nb!9@n1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
