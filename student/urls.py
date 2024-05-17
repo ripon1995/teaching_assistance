d5d7ac8abc001a907bedb13e3cb2fc4579625f7c
rootless <ripon1995dc@gmail.com>
2024-05-17 17:03:54 +0600

    Attendance
    - student-update view refactored

--- student/urls.py
+++ student/urls.py
@@ -5,6 +5,6 @@ from student.views import StudentCreateView, StudentRetrieveView, StudentAttenda
 urlpatterns = [
     path('student/', StudentCreateView.as_view()),
     path('student/<int:pk>/update/', StudentUpdateView.as_view()),
-    path('student/<int:student_id>/<int:course_id>/', StudentRetrieveView.as_view()),
+    path('student/<int:pk>/<int:course_id>/', StudentRetrieveView.as_view()),
     path('course/<int:course_id>/student/attendance/', StudentAttendanceListView.as_view()),
 ]
--- student/views.py
+++ student/views.py
@@ -16,15 +16,12 @@ class StudentRetrieveView(generics.RetrieveAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentRetrieveSerializer
 
-    def retrieve(self, request, *args, **kwargs):
-        student_id = self.kwargs.get('student_id')
-        course_id = self.kwargs.get('course_id')
-        student = Student.objects.filter(pk=student_id, course=course_id).first()
-        if student is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.get_serializer(student)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        student_id = self.kwargs['pk']
+        course_id = self.kwargs['course_id']
+        #TODO : implement filter class and filter backend
+        queryset = self.queryset.filter(pk=student_id, course_id=course_id)
+        return queryset
 
 
 class StudentUpdateView(generics.UpdateAPIView):
